92bd80333c8072a00aa37be8332d1c155aaa88f3
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-06-14 19:55:10 +0300

    change theme

--- src/app/TelegramProvider.tsx
+++ src/app/TelegramProvider.tsx
@@ -1,24 +1,22 @@
 'use client';
-
 import { useEffect } from 'react';
 import { init, themeParams } from '@telegram-apps/sdk-react';
 import { enableTelegramMock } from '@/lib/hooks/mockTelegramEnv';
-
 export default function TelegramProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     (async () => {
       if (process.env.NODE_ENV === 'development') {
         await enableTelegramMock();
       }
-
       await init();
       themeParams.mountSync();
-
-      window.Telegram?.WebApp?.onEvent?.('themeChanged', () => {
-        themeParams.mountSync();
-      });
+      const tg = window.Telegram?.WebApp;
+      if (tg?.onEvent) {
+        ['theme_changed', 'themeChanged'].forEach(evt =>
+          tg.onEvent(evt, () => themeParams.mountSync())
+        );
+      }
     })();
   }, []);
-
   return <>{children}</>;
 }
